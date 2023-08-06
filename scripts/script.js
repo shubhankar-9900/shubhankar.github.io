5f60c0bb8d13d1dcc96df1d7c093d168c78307a4
Shubhankar Valimbe <shubhankarvalimbe@gmail.com>
2023-08-06 07:33:05 -0500

    responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,32 @@
-{
-    const nav = document.querySelector(".nav");
-    let lastScrollY = window.scrollY;
+window.onload = () => {
+  const nav = document.querySelector(".navbar");
+  let lastScrollY = window.scrollY;
 
-    window.addEventListener("scroll", () => {
-        if (lastScrollY < window.scrollY) {
-            nav.classList.add("nav--hidden");
-        } else {
-            nav.classList.remove("nav--hidden");
-        }
+  window.addEventListener("scroll", () => {
+    var currScrollY = window.scrollY;
 
-        lastScrollY = window.scrollY;
-    });
-}
+    if (lastScrollY < currScrollY) {
+      nav.classList.add("hidden");
+    } else {
+      nav.classList.remove("hidden");
+    }
+
+    if (currScrollY === 0) {
+      nav.classList.add("at-top");
+      nav.classList.remove("scrolled");
+    } else {
+      nav.classList.add("scrolled");
+      nav.classList.remove("at-top");
+    }
+
+    lastScrollY = currScrollY;
+  });
+
+  const menu_btn = document.querySelector(".hamburger");
+  const mobile_menu = document.querySelector(".mobile-nav");
+
+  menu_btn.addEventListener("click", function () {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  });
+};
